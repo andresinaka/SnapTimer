998f7252f66539e5b1c2b75dda2b486ccccb8862
Andres Canal <andresyo990@gmail.com>
2016-08-20 17:40:47 -0300

    snaptimer view correctly rendering with scale from 0 to 100 for both inner and outer circles

---
+++ SnapTimer/SnapTimer.swift
@@ -0,0 +1,84 @@
+//
+//  SnapTimer.swift
+//  SnapTimer
+//
+//  Created by Andres on 8/18/16.
+//  Copyright © 2016 Andres. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class SnapTimer: UIView {
+	let pi: CGFloat = CGFloat(M_PI)
+	var snapCenter: CGPoint!
+	var radius: CGFloat!
+	let startAngle = 3/2 * CGFloat(M_PI)
+	let endAngle = 7/2 * CGFloat(M_PI)
+
+	@IBInspectable var mainBackgroundColor: UIColor = UIColor.darkGrayColor()
+	@IBInspectable var centerBackgroundColor: UIColor = UIColor.lightGrayColor()
+	@IBInspectable var outerBackgroundColor: UIColor = UIColor.whiteColor()
+	
+	@IBInspectable var outerValue: CGFloat = 0
+	@IBInspectable var innerValue: CGFloat = 0
+	
+	private func commonInit() {
+		self.snapCenter = CGPoint(x:bounds.width/2, y: bounds.height/2)
+		self.radius = min(bounds.width, bounds.height) * 0.5
+	}
+	
+    override func drawRect(rect: CGRect) {
+		self.commonInit()
+
+		self.mainCircle()
+		self.innerCompletion()
+		self.outterCompletion()
+    }
+	
+	private func radianForValue(value: CGFloat) -> CGFloat{
+		var realValue = value < 0 ? 0 : value
+		realValue = value > 100 ? 100 : value
+		
+		return (realValue * 4/2 * pi / 100) + self.startAngle
+	}
+
+	private func mainCircle() {
+		let bigCircle = UIBezierPath(arcCenter: self.snapCenter,
+		                             radius: self.radius,
+		                             startAngle: self.startAngle,
+		                             endAngle: self.endAngle,
+		                             clockwise: true)
+		
+		self.mainBackgroundColor.setFill()
+		bigCircle.fill()
+	}
+
+	private func innerCompletion() {
+		let startValue = self.radianForValue(self.innerValue)
+		
+		let innerCircle = UIBezierPath(arcCenter: self.snapCenter,
+		                               radius: self.radius / 2,
+		                               startAngle: startValue,
+		                               endAngle: self.endAngle,
+		                               clockwise: true)
+		innerCircle.addLineToPoint(self.snapCenter)
+		innerCircle.closePath()
+
+		self.centerBackgroundColor.setFill()
+		innerCircle.fill()
+	}
+
+	private func outterCompletion() {
+		let startValue = self.radianForValue(self.outerValue)
+
+		let outterCircle = UIBezierPath(arcCenter: self.snapCenter,
+		                                radius: self.radius * 0.75,
+		                                startAngle: startValue,
+		                                endAngle: self.endAngle,
+		                                clockwise: true)
+
+		self.outerBackgroundColor.setStroke()
+		outterCircle.lineWidth = radius * 0.35
+		outterCircle.stroke()
+	}
+}
---
+++ SnapTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapTimer
+//
+//  Created by Andres on 8/18/16.
+//  Copyright © 2016 Andres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
